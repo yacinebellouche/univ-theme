c42dc49d397cdb30e7077fe8559eb97b4cc1f85c
TheReaperxD <yacinebellouchexd@gmail.com>
2023-10-18 23:54:49 +0200

    Sevnth  Commit

--- single-program.php
+++ single-program.php
@@ -25,6 +25,57 @@ get_header();
     <div class="generic-content">
         <p> <?= the_content() ?> </p>
     </div>
+    <?php
+    $homepageEvents = new WP_Query(array(
+        'posts_per_page' => 2,
+        'post_type' => 'event',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_key' => 'event_date',
+        'meta_query' => array(
+            array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => date('Ymd'), //today
+                'type' => 'numeric'
+            ),
+            array(
+                'key' => 'related_program',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"'
+            )
+        )
+    ));
+    if ($homepageEvents->have_posts()) {
+        echo "<hr class='section-break' >";
+        echo '<h2 class="headline headline--medium" > Upcoming ' . get_the_title() . ' Events </h2>';
+
+        while ($homepageEvents->have_posts()) {
+            $homepageEvents->the_post();
+            $eventDate = new DateTime(get_field('event_date'));
+    ?>
+            <div class="event-summary">
+                <a class="event-summary__date t-center" href="<?= the_permalink() ?>">
+                    <span class="event-summary__month"><?= $eventDate->format('M') ?></span>
+                    <span class="event-summary__day"><?= $eventDate->format('d') ?></span>
+                </a>
+                <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a href="<?= the_permalink() ?>"><?= the_title() ?></a></h5>
+                    <p><?php
+                        if (has_excerpt()) {
+                            echo get_the_excerpt();
+                        } else {
+                            echo wp_trim_words(get_the_content(), 15);
+                        }; ?><a href="<?= the_permalink() ?>" class="nu gray">Learn more</a></p>
+                </div>
+            </div>
+
+    <?php
+        }
+    }
+
+    ?>
+
 </div>
 <?php
 
--- university-post-types.php
+++ university-post-types.php
@@ -5,15 +5,15 @@ function university_post_types()
 {
     // Event post type
     register_post_type('event', array(
-        'rewrite'=>array('slug'=>'events'),
-        'supports'=>array('excerpt','title','editor'),
-        'has_archive'=>true,
+        'rewrite' => array('slug' => 'events'),
+        'supports' => array('excerpt', 'title', 'editor'),
+        'has_archive' => true,
         'public' => true,
         'menu_icon' => 'dashicons-calendar',
-        'show_in_rest'=> true,
+        'show_in_rest' => true,
         'labels' => array(
-            'name'=>'Events',
-            'add_new_item' =>'Add New Event',
+            'name' => 'Events',
+            'add_new_item' => 'Add New Event',
             'edit_item' => 'Edit Event',
             'all_items' => 'All Events',
             'singular_name' => 'Event'
@@ -21,23 +21,34 @@ function university_post_types()
     ));
     //Program post tyoe
     register_post_type('program', array(
-        'rewrite'=>array('slug'=>'programs'),
-        'supports'=>array('title','editor'),
-        'has_archive'=>true,
+        'rewrite' => array('slug' => 'programs'),
+        'supports' => array('title', 'editor'),
+        'has_archive' => true,
         'public' => true,
         'menu_icon' => 'dashicons-awards',
-        'show_in_rest'=> true,
+        'show_in_rest' => true,
         'labels' => array(
-            'name'=>'Programs',
-            'add_new_item' =>'Add New Program',
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
             'edit_item' => 'Edit Program',
             'all_items' => 'All Programs',
             'singular_name' => 'Program'
         ),
     ));
-
-
-
+    //professor post type
+    register_post_type('professor', array(
+        'supports' => array('title', 'editor'),
+        'public' => true,
+        'menu_icon' => 'dashicons-businessperson',
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Professors',
+            'add_new_item' => 'Add New Professor',
+            'edit_item' => 'Edit Professor',
+            'all_items' => 'All Professor',
+            'singular_name' => 'Professor'
+        ),
+    ));
 }
 
 
